71c5538d656b624c2caa6fe8b8b8bd453d14d9c8
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2021-05-25 20:20:31 -0500

    Update stats.js

--- stats.js
+++ stats.js
@@ -7,8 +7,9 @@ socket.onmessage = function(event) {
   event = event.data.split(" ")
   for(let i = 0; i < 60; i+=3) {
     lbArray.push([event[i], event[i+1], event[i+2]])
-    document.getElementsByTagName("td")[i].innerHTML = lbArray[i/3][0]
-    document.getElementsByTagName("td")[i+1].innerHTML = lbArray[i/3][1]
+    for(let x = 0; x < 3; x++) {
+      document.getElementsByTagName("td")[i+x].innerHTML = lbArray[i/3][0+x]
+    }
   }
   console.log(lbArray)
 }
